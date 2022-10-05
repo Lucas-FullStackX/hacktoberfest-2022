c1cd6a77f0608ec3d3ab942d483bb52c699a5cea
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2022-10-04 21:42:48 -0500

    new styles

--- src/components/Lucas-FullStackX/Container.jsx
+++ src/components/Lucas-FullStackX/Container.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Form from './Form'
+import './styles.css'
 
 const Container = () => {
 	return (
-		<div className='w-full flex justify-center'>
+		<div className='w-full h-screen flex justify-center bg-stone justify-items-center items-center'>
 			<Form />
 		</div>
 	)
--- src/components/Lucas-FullStackX/Form.jsx
+++ src/components/Lucas-FullStackX/Form.jsx
@@ -4,7 +4,7 @@ import './styles.css'
 const Form = () => {
 	const slider = useRef(null)
 	const [value, setValue] = useState(6)
-	const [password, setPassword] = useState(0)
+	const [password, setPassword] = useState('TEST')
 
 	const rangeColor = (event) => {
 		setValue(event.target.value)
@@ -14,25 +14,31 @@ const Form = () => {
 		slider.current.style.backgroundSize = `${((val - min) * 100) / (max - min)}% 100%`
 	}
 	return (
-		<div className='w-1/2 mt-4 p-5 bg-mc-dark flex items-center flex-col'>
+		<div className='w-10/12 h-fit p-5 bg-zinc-800 flex items-center flex-col md:w-1/2'>
 			<h2 className='grass text-xl mb-8'>Password Generator</h2>
 			<div className='w-1/2 relative'>
 				<div className='absolute w-full flex items-center flex-col'>
 					<img className='w-1/3 floating-img mb-8' src='/Lucas-FullStackX/coal.png' alt='image' />
-					<div className='top-1/2 w-full bg-teal-500 bg-grass'>{password}</div>
+					<div className='top-1/2 w-full bg-white grass-border'>
+						<p className='grass text-center text-lg'>{password}</p>
+					</div>
 				</div>
 				<img src='/Lucas-FullStackX/table.png' alt='image' />
 			</div>
-			<input
-				onChange={rangeColor}
-				ref={slider}
-				type='range'
-				min='6'
-				max='30'
-				value={value}
-				className='range'
-			/>
-			<h2>{value}</h2>
+			<div className='flex mt-3 w-1/2'>
+				<input
+					onChange={rangeColor}
+					ref={slider}
+					type='range'
+					min='6'
+					max='30'
+					value={value}
+					className='range'
+				/>
+				<div className='bg-white grass-border-inter'>
+					<p className='to-black mr-2 ml-2'>{value}</p>
+				</div>
+			</div>
 			<button
 				onClick={() => {
 					setPassword(generatePassword(value))
